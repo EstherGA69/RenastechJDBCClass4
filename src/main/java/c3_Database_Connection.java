c6d5cbf9d86e7446588266193ddb2226d1803d09
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-12-23 07:47:28 +0530

    resultset previous demo

---
+++ src/main/java/c3_Database_Connection.java
@@ -0,0 +1,61 @@
+import java.sql.*;
+
+public class c3_Database_Connection {
+    public static void main(String[] args) throws SQLException {
+        // define the connectionstring
+        String connectionUrl = "jdbc:oracle:thin:@ec2-3-91-68-37.compute-1.amazonaws.com:1521:XE";
+        String userName = "hr";
+        String password = "hr";
+
+        // create connection
+        Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+
+        Statement statement= connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM countries");
+
+        // move cursor to 1st row in resultset
+        resultSet.next();
+
+        // print country name of 1st row
+        System.out.println("First Country Name: "+resultSet.getString("country_name"));
+
+        // move cursor to 2nd row in resultset
+        resultSet.next();
+        // print country name from 2nd row
+        System.out.println("Second Country Name: "+resultSet.getString("country_name"));
+
+        // row number 3
+        resultSet.next();
+        // row number 4
+        resultSet.next();
+        // row number 5
+        resultSet.next();
+        System.out.println("Fifth Country Name: "+resultSet.getString("country_name"));
+
+        // row number 4
+        resultSet.previous();
+        System.out.println("Fourth Country Name: "+resultSet.getString("country_name"));
+
+        // row number 3
+        resultSet.previous();
+        System.out.println("Third Country Name: "+resultSet.getString("country_name"));
+
+        // orw number 2
+        resultSet.previous();
+        System.out.println("Second Country Name: "+resultSet.getString("country_name"));
+
+        //row number 1
+        resultSet.previous();
+        System.out.println("First Country Name: "+resultSet.getString("country_name"));
+
+        resultSet.next();
+        System.out.println("Second Country Name: "+resultSet.getString("country_name"));
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+}
