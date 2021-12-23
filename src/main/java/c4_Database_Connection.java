65ba8f5ca778fa386989b3f25a2f4cff77d6ba3e
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-12-23 08:09:34 +0530

    added comments

--- src/main/java/c4_Database_Connection.java
+++ src/main/java/c4_Database_Connection.java
@@ -11,16 +11,29 @@ public class c4_Database_Connection {
         // create connection
         Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
 
+        // create statements. TYPE_SCROLL_INSENSITIVE will make our cursor free
         Statement statement= connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 
         ResultSet resultSet = statement.executeQuery("SELECT * FROM countries");
 
+        // check if cursor is after last row
         System.out.println(resultSet.isAfterLast());
+
+        // check if cursor is before first row
         System.out.println(resultSet.isBeforeFirst());
 
+        // move cursor to after last row
         resultSet.afterLast();
+        // this will print true since cursor is after last
         System.out.println(resultSet.isAfterLast());
+        // print false since cursor is after last
         System.out.println(resultSet.isBeforeFirst());
+
+        // move cursor to specific row:15
+        resultSet.absolute(15);
+        // print the country name from 15th row i.e. Japan
+        System.out.println("Fifteenth row: "+ resultSet.getString("country_name"));
+
         //close connection
         resultSet.close();
         statement.close();
