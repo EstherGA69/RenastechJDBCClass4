81fa40f1d9b5e4282b6587dbb5cfa9514bab6738
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-12-23 07:06:13 +0530

    read resultset by loop

--- src/main/java/c2_Database_Connection.java
+++ src/main/java/c2_Database_Connection.java
@@ -1,4 +1,33 @@
-
+import java.sql.*;
 
 public class c2_Database_Connection {
+    public static void main(String[] args) throws SQLException {
+        // define the connectionstring
+        String connectionUrl = "jdbc:oracle:thin:@ec2-3-91-68-37.compute-1.amazonaws.com:1521:XE";
+        String userName = "hr";
+        String password = "hr";
+
+        // create connection
+        Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+
+        // create statement
+        Statement statement = connection.createStatement();
+
+        //Resultset will store the executeQuery result
+        ResultSet resultset = statement.executeQuery("SELECT * FROM Employees");
+
+        resultset.next();
+        resultset.next();
+        System.out.println("First Employee name:"+resultset.getString("first_name"));
+
+        while(resultset.next()){
+            System.out.println("Employee Name: "+resultset.getString("first_name"));
+            System.out.println("Employee Email: "+resultset.getString("email"));
+        }
+        resultset.close();
+        statement.close();
+        connection.close();
+
+
+    }
 }
